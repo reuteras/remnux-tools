07c76c77e65390f6d32e94e3db2b9ddd476c0736
Peter Reuterås <peter@reuteras.com>
2016-03-06 19:56:43 +0100

    First real version of setup script.

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,10 +12,37 @@ sudo apt-get -y -qq install ctags curl git vim vim-doc vim-scripts \
 sudo apt-get -y -qq install open-vm-tools-desktop fuse
 
 if [ ! -d ~/src ]; then
-    mkdir ~/src
+    mkdir -p ~/src/bin
 fi    
 
 if [ ! -d ~/cases ]; then
     mkdir ~/cases
 fi    
+
+# Install Remnux
+if [[ ! -e ~/.config/.remnux ]]; then
+    wget --quiet -O - https://remnux.org/get-remnux.sh | sudo bash
+    touch ~/.config/.remnux
+fi
+
+# Install Sift
+if [[ ! -e ~/.config/.sift ]]; then
+    wget --quiet -O - https://raw.github.com/sans-dfir/sift-bootstrap/master/bootstrap.sh | sudo bash -s -- -i -s -y
+    touch ~/.config/.sift
+fi
+
+# Install Chrome
+if ! dpkg --status google-chrome-stable > /dev/null 2>&1 ; then
+    cd /tmp
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb || true
+    sudo apt-get -f install
+    rm -f google-chrome-stable_current_amd64.deb
+fi
+
+# This repo contians newer versions of Wireshark etc. Update again after adding
+if ! grep phi-rho/security /etc/apt/sources.list > /dev/null ; then
+    sudo add-apt-repository ppa:pi-rho/security
+    sudo apt-get update && sudo apt-get -y dist-upgrade
+fi
 
