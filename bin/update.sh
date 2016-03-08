518d02c31791f5c472911ea4fc7914ed54ca24d4
Peter Reuterås <peter@reuteras.net>
2016-03-08 16:41:00 +0100

    Fix google apt source if google update destroys it.

--- bin/update.sh
+++ bin/update.sh
@@ -5,5 +5,9 @@ set -e
 sudo update-remnux
 sudo update-sift
 
+if ! grep "arch=amd64" /etc/apt/sources.list.d/google-chrome.list > /dev/null ; then
+    sudo sed -i "s/deb http/deb [arch=amd64] http/" /etc/apt/sources.list.d/google-chrome.list
+fi
+
 sudo apt-get update && sudo apt-get -y dist-upgrade
 
