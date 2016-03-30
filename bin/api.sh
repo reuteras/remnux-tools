a0e98ddf6a81a89c72562b452b8910bd294a45b9
Peter Reuterås <peter@reuteras.com>
2016-03-30 21:39:45 +0200

    First api function for Shodan.

---
+++ bin/api.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -e ~/remnux-tools/config.cfg ]; then
+    echo "Can't find ~/remnux-tools/config.cfg."
+    exit 1
+fi
+
+# shellcheck disable=SC1090
+. ~/remnux-tools/config.cfg
+
+if [[ ! -e ~/shodan/api_key && ! -z "$SHODAN_API_KEY" ]]; then
+    echo -n "iInitialize Shodan API (access via command shodan): "
+    shodan init "$SHODAN_API_KEY"
+fi
