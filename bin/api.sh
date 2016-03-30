a438bf09f79d28002bdf999c4ba493c341ef05ed
Peter Reuterås <peter@reuteras.com>
2016-03-30 21:42:23 +0200

    Two errors on line...

--- bin/api.sh
+++ bin/api.sh
@@ -8,7 +8,7 @@ fi
 # shellcheck disable=SC1090
 . ~/remnux-tools/config.cfg
 
-if [[ ! -e ~/shodan/api_key && ! -z "$SHODAN_API_KEY" ]]; then
+if [[ ! -e ~/.shodan/api_key && ! -z "$SHODAN_API_KEY" ]]; then
     echo -n "Initialize Shodan API (access via command shodan): "
     shodan init "$SHODAN_API_KEY"
 fi
