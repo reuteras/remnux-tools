42596e6cf2cc66f34a521d3b22433dd0133a41ad
Peter Reuterås <peter@reuteras.com>
2016-10-23 00:08:02 +0200

    New script to install SIFT
    * Removed most addons installed in the Remnux version. Keep SIFT clean
    from the start.

---
+++ bin/setup-sift.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# shellcheck source=/dev/null
+[[ -e ~/remnux-tools/bin/common.sh ]] && . ~/remnux-tools/bin/common.sh || exit "Cant find common.sh."
+
+sudo apt-get update && sudo apt-get -y dist-upgrade
+
+install-general-tools
+install-vmware-tools
+
+if [ ! -d ~/cases ]; then
+    mkdir -p ~/cases/
+fi
+
+# Install SIFT
+if [[ ! -e ~/.config/.sift ]]; then
+    wget --quiet -O - https://raw.github.com/sans-dfir/sift-bootstrap/master/bootstrap.sh | sudo bash -s -- -i -s -y
+    touch ~/.config/.sift
+fi
+
+install-google-chrome
+
+# Clean up
+if [[ -e ~/examples.desktop ]]; then
+    rm -f ~/examples.desktop
+fi
+
+# Turn off sound on start up
+turn-of-sound
+
+# Info manual config
+if [[ ! -e ~/.config/.manual_conf ]]; then
+    echo "##################################################################"
+    echo "Remember to change the following things:"
+    echo "1. Change desktop resolution to be able to do the other steps."
+    echo "2. Security & Privacy -> Search -> Turn of online search results."
+    echo "3. -> Diagnotstics -> Turn of error reports."
+    echo "Run make dotfiles in ~/remnux-tools for .bashrc etc."
+    echo "##################################################################"
+    touch ~/.config/.manual_conf
+fi
+
