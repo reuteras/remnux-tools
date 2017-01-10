b8498e14d3d436ab2c7a5a94c2ad04e4800806ba
Peter Reuterås <peter@reuteras.com>
2017-01-10 20:57:48 +0100

    Send output to logfile.

--- bin/setup-remnux.sh
+++ bin/setup-remnux.sh
@@ -2,6 +2,7 @@
 
 set -e
 LOG=/tmp/remnux-tools.log
+touch $LOG
 
 # shellcheck source=/dev/null
 [[ -e ~/remnux-tools/bin/common.sh ]] && . ~/remnux-tools/bin/common.sh || exit "Cant find common.sh."
@@ -11,10 +12,11 @@ info-message "Updating Ubuntu."
 sudo apt-get -qq update && sudo apt-get -qq -y dist-upgrade
 
 info-message "Installing general tools."
-install-general-tools > $LOG
+install-general-tools >> $LOG 2&>1
 info-message "Installing tools for VMware."
-install-vmware-tools >> $LOG
+install-vmware-tools >> $LOG 2&>1
 
+info-message "Create directory structure."
 if [ ! -d ~/src ]; then
     mkdir -p ~/src/bin
 fi
@@ -40,71 +42,61 @@ done
 
 # Install Remnux
 if [[ ! -e ~/.config/.remnux ]]; then
+    info-message "Start installation of Remnux."
     wget --quiet -O - https://remnux.org/get-remnux.sh | sudo bash
     touch ~/.config/.remnux
+    info-message "Remnux installation finished."
 fi
 
-install-google-chrome
+info-message "Install Google Chrome."
+install-google-chrome >> $LOG 2>&1
 
-# This repo contians newer versions of Wireshark etc. Update again after adding
-if [[ ! -e /etc/apt/sources.list.d/pi-rho-security-trusty.list ]]; then
-    sudo add-apt-repository -y ppa:pi-rho/security
-    sudo apt-get -qq update && sudo apt-get -qq -y dist-upgrade
-    sudo apt-get -qq -y install html2text nasm && sudo apt-get autoremove -qq -y
-fi
+info-message "Enable ppa:pi-rho/security and install updated packages."
+install-pi-rho-security >> $LOG 2>&1
 
-# Clean up
-if [[ -e ~/examples.desktop ]]; then
-    rm -f ~/examples.desktop
-fi
-if [[ -e ~/Desktop/SANS-DFIR.pdf ]]; then
-    echo "Clean Desktop."
-    mkdir ~/Documents/Remnux_SIFT
-    mv ~/Desktop/REMnux* ~/Documents/Remnux_SIFT/
-    mv ~/Desktop/*.pdf ~/Documents/Remnux_SIFT/
-    rm -f ~/Desktop/cases
-fi
+info-message "Clean up folders and files."
+cleanup >> $LOG 2>&1
 
 # Add single file scripts from different sources
 # http://phishme.com/powerpoint-and-custom-actions/
 [ ! -e ~/src/bin/psparser.py ] && wget -q -O ~/src/bin/psparser.py \
-    https://github.com/phishme/malware_analysis/blob/master/scripts/psparser.py && \
+    https://github.com/phishme/malware_analysis/blob/master/scripts/psparser.py >> $LOG 2>&1 && \
     chmod +x ~/src/bin/psparser.py && \
     info-message "Installed psparser.py"
 # https://www.virustotal.com/en/documentation/public-api/#getting-file-scans
 [ ! -e ~/src/bin/vt.py ] && wget -q -O ~/src/bin/vt.py \
-    https://raw.githubusercontent.com/Xen0ph0n/VirusTotal_API_Tool/master/vt.py && \
+    https://raw.githubusercontent.com/Xen0ph0n/VirusTotal_API_Tool/master/vt.py >> $LOG 2>&1 && \
     chmod +x ~/src/bin/vt.py && \
     info-message "Installed vt.py."
 # https://testssl.sh/
 [ ! -e ~/src/bin/testssl.sh ] && wget -q -O ~/src/bin/testssl.sh \
-    https://testssl.sh/testssl.sh && \
+    https://testssl.sh/testssl.sh >> $LOG 2>&1 && \
     chmod +x ~/src/bin/testssl.sh && \
     info-message "Installed testssl.sh."
 # Fireeye floss
 [ ! -e ~/src/bin/floss ] && wget -q -O ~/src/bin/floss \
-    https://s3.amazonaws.com/build-artifacts.floss.flare.fireeye.com/travis/linux/dist/floss && \
+    https://s3.amazonaws.com/build-artifacts.floss.flare.fireeye.com/travis/linux/dist/floss >> $LOG 2>&1 && \
     chmod +x ~/src/bin/floss && \
     info-message "Installed floss."
 
+# Install pip lib globally
+# shellcheck disable=SC2024
+sudo -H pip install colorclass >> $LOG 2>&1
+info-message "Installed python colorclass."
+
+info-message "Setup virtualenvwrapper."
 # Use virtualenvwrapper for python tools
 export PROJECT_HOME="$HOME"/src/python
 # shellcheck source=/dev/null
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
-# Make sure pip is up to date
-sudo -H pip install --upgrade pip
-
-# Install pip lib globally
-sudo -H pip install colorclass
-
 # Add git repos
 # http://www.tekdefense.com/automater/
 [ ! -d ~/src/python/automater ] && \
     git clone --quiet https://github.com/1aN0rmus/TekDefense-Automater.git \
-        ~/src/python/automater && \
+        ~/src/python/automater >> $LOG 2>&1 && \
     cd ~/src/python/automater && \
-    mkvirtualenv automater && \
+    mkvirtualenv automater >> $LOG 2>&1 && \
     setvirtualenvproject && \
     deactivate && \
     info-message "Checked out Automater."
@@ -115,13 +107,13 @@ sudo -H pip install colorclass
 [ ! -d ~/src/python/damm ] && \
     mkdir -p ~/src/python/damm && \
     git clone --quiet https://github.com/504ensicsLabs/DAMM \
-        ~/src/python/damm/damm && \
+        ~/src/python/damm/damm >> $LOG 2>&1 && \
     cd ~/src/python/damm/damm && \
-    mkvirtualenv damm && \
+    mkvirtualenv damm >> $LOG 2>&1 && \
     setvirtualenvproject && \
-    pip install --upgrade pip && \
-    pip install --upgrade urllib3[secure] && \
-    install-volatility ~/src/python/damm/volatility && \
+    pip install --upgrade pip >> $LOG 2>&1 && \
+    pip install --upgrade urllib3[secure] >> $LOG 2>&1 && \
+    install-volatility ~/src/python/damm/volatility >> $LOG 2>&1 && \
     deactivate && \
     info-message "Checked out DAMM."
 
@@ -129,16 +121,16 @@ sudo -H pip install colorclass
 # shellcheck disable=SC2102
 [ ! -d ~/src/python/volatility ] && \
     mkdir -p ~/src/python/volutility && \
-    mkvirtualenv volutility && \
+    mkvirtualenv volutility >> $LOG 2>&1 && \
     echo "Start MongoDB with docker-mongodb" > ~/src/python/volutility/README && \
-    pip install --upgrade pip && \
-    pip install --upgrade urllib3[secure] && \
-    install-volatility ~/src/python/volutility/volatility && \
+    pip install --upgrade pip >> $LOG 2>&1 && \
+    pip install --upgrade urllib3[secure] >> $LOG 2>&1 && \
+    install-volatility ~/src/python/volutility/volatility >> $LOG 2>&1 && \
     git clone https://github.com/kevthehermit/VolUtility \
-        ~/src/python/volutility/volutility && \
+        ~/src/python/volutility/volutility >> $LOG 2>&1 && \
     cd ~/src/python/volutility/volutility && \
-    pip install -r requirements.txt && \
-    pip install virustotal-api yara-python && \
+    pip install -r requirements.txt >> $LOG 2>&1 && \
+    pip install virustotal-api yara-python >> $LOG 2>&1 && \
     deactivate && \
     info-message "Installed Volutility."
 
@@ -146,19 +138,19 @@ sudo -H pip install colorclass
 # shellcheck disable=SC2102
 [ ! -d ~/src/python/volatility ] && \
     mkdir -p ~/src/python/volatility && \
-    mkvirtualenv volatility && \
-    pip install --upgrade pip && \
-    pip install --upgrade urllib3[secure] && \
-    install-volatility ~/src/python/volatility/volatility && \
+    mkvirtualenv volatility >> $LOG 2>&1 && \
+    pip install --upgrade pip >> $LOG 2>&1 && \
+    pip install --upgrade urllib3[secure] >> $LOG 2>&1 && \
+    install-volatility ~/src/python/volatility/volatility >> $LOG 2>&1 && \
     deactivate && \
     info-message "Checked out Volatility."
 
 # https://github.com/DidierStevens/DidierStevensSuite
 [ ! -d ~/src/python/didierstevenssuite ] && \
     git clone --quiet https://github.com/DidierStevens/DidierStevensSuite.git \
-    ~/src/python/didierstevenssuite && \
-    mkvirtualenv didierstevenssuite && \
-    setvirtualenvproject && \
+        ~/src/python/didierstevenssuite >> $LOG 2>&1 && \
+    mkvirtualenv didierstevenssuite >> $LOG 2>&1 && \
+    setvirtualenvproject >> $LOG 2>&1 && \
     deactivate && \
     enable-new-didier && \
     info-message "Checked out DidierStevensSuite."
@@ -166,30 +158,18 @@ sudo -H pip install colorclass
 # https://github.com/decalage2/oletools.git
 # shellcheck disable=SC2102
 [ ! -d ~/.virtualenvs/oletools ] && \
-    mkvirtualenv oletools && \
-    pip install --upgrade pip && \
-    pip install --upgrade urllib3[secure] && \
-    pip install oletools && \
+    mkvirtualenv oletools >> $LOG 2>&1 && \
+    pip install --upgrade pip >> $LOG 2>&1 && \
+    pip install --upgrade urllib3[secure] >> $LOG 2>&1 && \
+    pip install oletools >> $LOG 2>&1 && \
     info-message "Installed oletools."
-
-# Fix problem with pip - https://github.com/pypa/pip/issues/1093
-#[ ! -e /usr/local/bin/pip ] && \
-#    sudo apt-get remove -yqq --auto-remove python-pip && \
-#    wget --quiet -O /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py && \
-#    sudo -H python /tmp/get-pip.py && \
-#    sudo ln -s /usr/local/bin/pip /usr/bin/pip && \
-#    sudo rm /tmp/get-pip.py && \
-#    sudo -H pip install pyopenssl ndg-httpsclient pyasn1 && \
-#    info-message "Install pip from pypa.io."
 
 # Python virtualenv
 # Checkout Rekall to fix problem with python-dateutil being newer.
 [ ! -d ~/.virtualenvs/rekall ] && \
-    mkvirtualenv rekall && \
-    echo -n "Update pip and setuptools for rekall." && \
-    pip install -U pip setuptools && \
-    echo -n "Start installation of rekall." && \
-    pip install rekall rekall-gui > /dev/null && \
+    mkvirtualenv rekall >> $LOG 2>&1 && \
+    pip install -U pip setuptools >> $LOG 2>&1 && \
+    pip install rekall rekall-gui >> $LOG 2>&1 && \
     deactivate && \
     echo " Done."
 
@@ -197,14 +177,15 @@ sudo -H pip install colorclass
 # https://github.com/keydet89/RegRipper2.8
 [ ! -d ~/src/git/RegRipper2.8 ] && \
     git clone --quiet https://github.com/keydet89/RegRipper2.8.git \
-        ~/src/git/RegRipper2.8 && \
+        ~/src/git/RegRipper2.8 >> $LOG 2>&1 && \
     info-message "Checked out RegRipper2.8." && \
     cp ~/remnux-tools/files/regripper2.8 ~/src/bin/regripper2.8 && \
     chmod 755 ~/src/bin/regripper2.8
 
 # https://github.com/Yara-Rules/rules.git
 [ ! -d ~/src/git/rules ] && \
-    git clone --quiet https://github.com/Yara-Rules/rules.git ~/src/git/rules && \
+    git clone --quiet https://github.com/Yara-Rules/rules.git \
+        ~/src/git/rules >> $LOG 2>&1 && \
     info-message "Checked out Yara-Rules."
 
 # Turn off sound on start up
