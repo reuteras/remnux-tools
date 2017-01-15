dbd70f25005c0083d3ced7212aaee84e11305df2
Peter Reuterås <peter@reuteras.com>
2017-01-15 20:02:14 +0100

    One command per line.

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo apt-get -y autoremove && sudo apt-get autoclean && \
-    sudo apt-get clean
+sudo apt-get -y autoremove
+sudo apt-get autoclean
+sudo apt-get clean
+rm -f /tmp/remnux-tools.log
 
-dd if=/dev/zero of="$HOME/zero" conv=fsync; sleep 1 ; sync; rm -f "$HOME"/zero
-
+dd if=/dev/zero of="$HOME/zero" conv=fsync
+sleep 1
+sync
+rm -f "$HOME"/zero
