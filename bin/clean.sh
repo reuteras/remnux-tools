9a06728832905aa030d1cdf3b9d7bb715b664a3b
Peter Reuterås <peter@reuteras.com>
2017-01-15 14:45:10 +0100

    Automatically remove packages.

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get autoremove && sudo apt-get autoclean && \
+sudo apt-get -y autoremove && sudo apt-get autoclean && \
     sudo apt-get clean
 
 dd if=/dev/zero of="$HOME/zero" conv=fsync; sleep 1 ; sync; rm -f "$HOME"/zero
