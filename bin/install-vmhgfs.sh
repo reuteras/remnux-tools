320d93b04a48ea016441bd9f3113ecd361787c9e
Peter Reuterås <peter@reuteras.com>
2017-01-16 20:48:59 +0100

    Add script to install vmhgfs module.

---
+++ bin/install-vmhgfs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+LOG=/tmp/remnux-tools.log
+touch "$LOG"
+
+# shellcheck source=/dev/null
+[[ -e ~/remnux-tools/bin/common.sh ]] && . ~/remnux-tools/bin/common.sh || exit "Cant find common.sh."
+
+if [[ ! -f ~/Desktop/linux.iso ]]; then
+    info-message "You have to copy linux.iso from your VMware installation to ~/Desktop/linux.iso."
+    info-message "On Macos open /Applications and right click on VMware Fusion. You'll find the linux.iso under Contents/Library/isoimages."
+    exit 1
+fi
+
+info-message "Starting installation of VMware vmhgfs module with remnux-tools."
+info-message "Details logged to $LOG."
+
+TMP_DIR=$(mktemp -d)
+CURRENT_DIR=$PWD
+info-message "Created TMP_DIR: $TMP_DIR"
+
+info-message "Mount linux.iso"
+sudo mount --read-only ~/Desktop/linux.iso /mnt/cdrom
+cd $TMP_DIR || exit "Couldn't cd to $TMP_DIR"
+info-message "Extract VMwareTools."
+tar zxf /mnt/cdrom/VMwareTools*.tar.gz
+info-message "Umount linux.iso"
+sudo umount /mnt/cdrom
+cd vmware-tools-distrib || exit "Couldn't cd to vmware-tools-distrib."
+
+info-message "Start vmware-install.pl."
+sudo ./vmware-install.pl -d >> "$LOG" 2>&1
+info-message "Installation completed."
+
+info-message "Remove TMP_DIR: $TMP_DIR"
+cd $CURRENT_DIR
+rm -rf $TMP_DIR
+
+if [[ "$(vmware-hgfsclient)" != "" ]]; then
+    info-message "Run the following command to mount your share on /cases:"
+    info-message "sudo mount -t vmhgfs .host:/$(vmware-hgfsclient) /cases"
+fi
+
+info-message "Stopping open-vm-tools."
+sudo service open-vm-tools stop >> "$LOG" 2>&1
+info-message "Starting open-vm-tools."
+sudo service open-vm-tools stop >> "$LOG" 2>&1
+info-message "You still might need to reboot to get copy and paste to work."
+info-message "Installation completed."
