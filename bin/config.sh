8736803035a47e3089667d67d6f83e004255243a
Peter Reuterås <peter@reuteras.com>
2017-01-28 09:41:38 +0100

    Needed ""

--- bin/config.sh
+++ bin/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 dconf write /com/canonical/unity/launcher/favorites "['application://gnome-terminal.desktop', 'application://nautilus.desktop', 'application://google-chrome.desktop', 'application://firefox.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
-dconf write /com/canonical/unity/lenses/remote-content-search 'none'
+dconf write /com/canonical/unity/lenses/remote-content-search "'none'"
 dconf write /org/gnome/desktop/screensaver/ubuntu-lock-on-suspend false
 dconf write /org/gnome/desktop/screensaver/lock-enabled false
